bead8dd3a3f40b53ca0ec75e7c864b03e777757f
Egor Subbotin <subbotin.egor.a@gmail.com>
2019-08-01 21:51:39 +0300

    начало cleancode

---
+++ src/main/java/com/epam/automation/cleancode/models/ClassificationLevel.java
@@ -0,0 +1,5 @@
+package com.epam.automation.cleancode.models;
+
+public enum ClassificationLevel {
+    UNCLASSIFIED, CONFIDENTIAL, SECRET, TOP_SECRET
+}
---
+++ src/main/java/com/epam/automation/cleancode/models/MilitaryType.java
@@ -0,0 +1,5 @@
+package com.epam.automation.cleancode.models;
+
+public enum MilitaryType {
+    FIGHTER, BOMBER, TRANSPORT
+}
